7e175669c4d768fa69d546c1a69712c440a3cb4b
mceranic1 <mceranic1@etf.unsa.ba>
2017-05-17 00:16:31 +0100

    Dodane forme

--- Projekat/KrimTim/Administrator.xaml.cs
+++ Projekat/KrimTim/Administrator.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Blank
+namespace Blank.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Administrator : Page
     {
-        public MainPage()
+        public Administrator()
         {
             this.InitializeComponent();
         }
